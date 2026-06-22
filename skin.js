feee9c7e79c7942f5028491ec2c59a211ec0dd4c
Laurence421 <onanad.johnlaurence.a@gmail.com>
2026-06-22 21:39:47 +0800

    mobile navbaar

---
+++ skin.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('#menuBtn');
+const items = document.querySelector('#menu');
+
+menuBtn.addEventListener('click', () =>{
+    items.classList.toggle('open-items');
+});
+items.addEventListener('click', () => {
+    items.classList.remove('open-items')
+});
