4f08f3fe91609cf6e5b1e5c37b234e48ea549c6b
Laurence421 <onanad.johnlaurence.a@gmail.com>
2026-07-02 22:56:22 +0800

    mobile main content done

--- skin.js
+++ skin.js
@@ -8,3 +8,27 @@ items.addEventListener('click', () => {
     items.classList.remove('open-items')
 });
 
+const timer = document.querySelector('#clock');
+
+let totalSeconds = 12 * 30 * 56;
+function update(){
+    let hr = Math.floor((totalSeconds / 3600));
+    let mn = Math.floor((totalSeconds % 3600 / 60));
+    let sec = totalSeconds % 60;
+
+    hr = String(hr).padStart(2,'0');
+    mn = String(mn).padStart(2,'0');
+    sec = String(sec).padStart(2,'0');
+
+    timer.textContent = `${hr}hrs ${mn}mins ${sec}sec`;
+}
+
+const timerId = setInterval (() =>{
+    if (totalSeconds <= 0) {
+        clearInterval(timerId);
+        timer.textContent = "Time's up!";
+        return
+    }
+    totalSeconds--;
+    update();
+}, 1000);
